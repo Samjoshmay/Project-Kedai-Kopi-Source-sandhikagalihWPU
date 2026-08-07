3636437aa19a5a23979c3a8005cc22d6c169c456
Samjoshmay <sugiyartiniwanget9@gmail.com>
2026-08-08 00:14:30 +0700

    Product Cart Part

--- js/script.js
+++ js/script.js
@@ -5,11 +5,27 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
+// Toggle class active
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  // searchBox.focus biar langsung tulis tanpa klik
+  e.preventDefault();
+  // biar kalau pencet search nggak balik ke paling atas dan harus masukin e ke onclick = ()
+};
+
 // Klik di luar sidebar untuk menghilangkan nav
 const hamburger = document.querySelector("#hamburger-menu");
+const Sb = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (!Sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
