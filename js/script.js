442114b7b6e258bb7c8b240f23a79ebda4574cc4
Samjoshmay <sugiyartiniwanget9@gmail.com>
2026-08-08 23:30:47 +0700

    Final Commit: Shopping cart and item detail

--- js/script.js
+++ js/script.js
@@ -17,9 +17,16 @@ document.querySelector("#search-button").onclick = (e) => {
   // biar kalau pencet search nggak balik ke paling atas dan harus masukin e ke onclick = ()
 };
 
+const shoppingCart = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
 // Klik di luar sidebar untuk menghilangkan nav
 const hamburger = document.querySelector("#hamburger-menu");
 const Sb = document.querySelector("#search-button");
+const Scb = document.querySelector("#shopping-cart-button");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
@@ -28,4 +35,31 @@ document.addEventListener("click", function (e) {
   if (!Sb.contains(e.target) && !searchForm.contains(e.target)) {
     searchForm.classList.remove("active");
   }
+  if (!Scb.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
 });
+
+// Modal Box
+
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButton = document.querySelectorAll(".item-detail-button");
+
+itemDetailButton.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// // klik tombol close modal
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
